89ee8f7ffe9ad3f13f01518452a0a4e23867dcbe
zakkaiProxy <zackdavidzepeda@gmail.com>
2025-08-08 18:15:57 -0600

    header

---
+++ Character.h
@@ -0,0 +1,25 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include "raylib.h"
+#include "BaseCharacter.h"
+
+
+class Character : public BaseCharacter
+{
+public:
+    Character(int winWidth, int winHeight);
+    virtual void tick(float deltaTime) override;
+    virtual Vector2 getScreenPos() override;
+    Rectangle getWeaponCollisionRec() {return weaponCollisionRec;}
+    float getHealth() const {return health;}
+    void takeDamage(float damage);
+private:
+    int windowWidth{};
+    int windowHeight{};
+    Texture2D weapon{LoadTexture("/home/zakku/classy-clash/characters/weapon_sword.png")};
+    Rectangle weaponCollisionRec{};
+    float health{100.f};
+};
+
+#endif
